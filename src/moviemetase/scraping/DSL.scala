dc64a117b1b33b522b0d196f59a2b3762bef9051
Kai H. Meder <kai@meder.info>
2012-08-10 00:08:56 +0200

    scraping DSL ...

--- src/moviemetase/scraping/DSL.scala
+++ src/moviemetase/scraping/DSL.scala
@@ -1,181 +1,98 @@
 package moviemetase
 package scraping
 
-import scala.util.parsing.combinator.syntactical.StandardTokenParsers
-import java.net.URL
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
+import scala.util.parsing.combinator.RegexParsers
+import scala.util.parsing.input.CharSequenceReader
+import scala.util.parsing.combinator.PackratParsers
 
-case class Context[A](results: List[A], idents: Map[String, String]) {
-  def add(result: A): Context[A] = Context(result :: results, idents)
+object DSL extends RegexParsers with PackratParsers {
   
-  def ident(id: String): String = idents.get(id) match {
-    case Some(v) => v
-    case None => throw new Exception("Invalid Identifier '" + id + "'")
-  }
-}
-
-trait Step[A] {
-  def process(elem: Element, ctx: Context[A]): List[A]
-}
-
-case class TerminalStep[A]() extends Step[A] {
-  def process(elem: Element, ctx: Context[A]): List[A] = ctx.results
-}
-
-case class BrowseStep[A](expr: Expr, next: Step[A]) extends Step[A] with Logging {
-  val logID = "Browse"
+  override protected val whiteSpace = """(\s|#.*)+""".r
   
-  def browse(theUrl: URL): Document = new HtmlTask[Document] {
-    def url = theUrl
-    def processDocument(doc: Document): Document = doc
-  }.execute()
-
-  def process(elem: Element, ctx: Context[A]): List[A] = {
-    val value = Expr.apply(expr, elem, ctx)
-    val url = new URL(value)
-    trace("browsing '" + url + "'")
-    val doc = browse(url)
-    next.process(doc.body, ctx)
-  }
-}
-
-case class SelectStep[A](selector: String, next: Step[A]) extends Step[A] with Logging {
-  import language.implicitConversions
-  implicit def jiter[A](jiter: java.util.Iterator[A]): Iterator[A] =
-    scala.collection.convert.Wrappers.JIteratorWrapper[A]( jiter )
- 
-  val logID = "Select"
+  lazy val quoted   = "\"" ~> """[^"]*""".r <~ "\""
+  lazy val unquoted = """\S+""".r
+  lazy val value    = quoted | unquoted
+  
+  lazy val int = """-?[0-9]+""".r ^^ { d => d.toInt }
+  
+  lazy val keyword = """[A-Z\-_]+""".r
     
-  def process(elem: Element, ctx: Context[A]): List[A] = {
-    val selElems = elem.select(selector)
-    trace("selected '" + selector + "': " + selElems.size)
-    
-    selElems.iterator.flatMap(selElem => next.process(selElem, ctx)).toList
-  }
-}
-
-trait ExtractorFactory[A] {
-  def create(what: String, value: String): A
-}
-
-case class ExtractStep[A](what: String, expr: Expr, factory: ExtractorFactory[A], next: Step[A]) extends Step[A] with Logging {
-  val logID = "Extract(" + what + ")"
   
-  def process(elem: Element, ctx: Context[A]): List[A] = {
-    val value = Expr.apply(expr, elem, ctx)
-    val result = factory.create(what, value)
-    trace("extracted '" + value + "': " + result)
-
-    next.process(elem, ctx add result)
-  }
-}
-
-case class Scraper[A](site: String, author: String, start: Step[A]) extends Logging {
-  val logID = "Scrape(" + site + ")"
+  // ############################################
+  // expressions
+  lazy val literalExpr = value ^^ { case s => LiteralExpr(s) }
   
-  def scrape(query: String): List[A] = {
-    val html = """<html><head></head><body></body></html>"""
-    val doc = org.jsoup.Jsoup.parse(html)
-    
-    val idents = Map[String, String](
-      "QUERY" -> query
-    )
-    trace("identifiers: " + idents.mkString)
-
-    val ctx = Context[A](Nil, idents)
-    
-    start.process(doc.body, ctx)
-  }
-}
-
-sealed trait Expr
-  case class LiteralExpr(s: String) extends Expr
-  case class SelectExpr(selector: String) extends Expr
-  case class AttributeExpr(name: String) extends Expr
-  case class SelectAttributeExpr(selector: String, name: String) extends Expr
-  case class ConcatExpr(a: Expr, b: Expr) extends Expr
-  case class IdentExpr(n: String) extends Expr // e.g. "SEARCH-TERM", better name?!
-  // TODO RegexExpr
-  // ...
-
-object Expr {
-  import language.implicitConversions
-  implicit def jiter[A](jiter: java.util.Iterator[A]): Iterator[A] =
-    scala.collection.convert.Wrappers.JIteratorWrapper[A]( jiter )
+  lazy val selectExpr = "SELECT" ~> value ^^ { case s => SelectExpr(s) }
   
-  def apply(e: Expr, elem: Element, ctx: Context[_]): String = e match {
-    case LiteralExpr(s) => s
-    case SelectExpr(s) => {
-      val elems = elem.select(s)
-      if (elems.size == 0) ""
-      else elems.get(0).text
+  lazy val attributeExpr = "ATTRIBUTE" ~> value ^^ { case s => AttributeExpr(s) }
+  
+  lazy val selectAttributeExpr = "SELECT-ATTRIBUTE" ~> value ~ value ^^ { case s ~ a => SelectAttributeExpr(s, a) }
+    
+  lazy val identExpr = "<" ~> """[A-Z]""".r <~ ">" ^^ { case s => IdentExpr(s) }
+  
+  //def nonConcatExpr = literalExpr | selectExpr | attributeExpr | selectAttributeExpr
+  //def concatExpr = nonConcatExpr ~ "+" ~ expr ^^ { case e1 ~ _ ~ e2 => ConcatExpr(e1, e2) }
+  //def expr: Parser[Expr] = concatExpr | nonConcatExpr
+  
+  lazy val concatExpr: PackratParser[Expr] = expr ~ "+" ~ expr ^^ { case e1 ~ _ ~ e2 => ConcatExpr(e1, e2) }
+  lazy val expr: PackratParser[Expr] = concatExpr | literalExpr | selectExpr | attributeExpr | selectAttributeExpr
+  
+    
+  // ############################################
+  // steps 
+  lazy val browseStep: PackratParser[Step[_]] = "BROWSE"  ~> expr ~ step ^^ { case e ~ next => BrowseStep(e, next) }
+  
+  lazy val selectStep: PackratParser[Step[_]] = "SELECT"  ~> value /* ~ ("MAX" ~> int | Int.MaxValue ) */ ~ step ^^ { case e ~ next => SelectStep(e, Int.MaxValue, next) }
+  
+  lazy val extractStep: PackratParser[Step[_]] = "EXTRACT" ~> keyword ~ expr ~ step ^^ { case what ~ e ~ next => ExtractStep(what, e, next) }
+  
+  lazy val terminalStep: PackratParser[Step[_]] = "END" ^^^ TerminalStep()
+  
+  lazy val step: PackratParser[Step[_]] = browseStep | selectStep | extractStep | terminalStep
+
+  
+  // ############################################
+  // scrapers
+  
+  lazy val subtitlesScraper: PackratParser[Scraper[_]] = "SCRAPE" ~> "SUBTITLES" ~> "AT" ~> value ~ "BY" ~ value ~ step ^^ { case site ~ _ ~ author ~ step => SubtitleScraper(site, author, step.asInstanceOf[Step[MovieInfos.Subtitle]]) }  
+  
+  lazy val scraper: PackratParser[Scraper[_]] = subtitlesScraper
+
+  lazy val scrapers = rep1(scraper)
+  
+  def apply(s: String): List[Scraper[_]] =
+    parseAll(scrapers, s) match {
+      case Success(t, _)     => t
+      case f@NoSuccess(msg,_) => {
+        println(f)
+        Nil
+      }
     }
-    case AttributeExpr(a) =>
-      elem.attr(a)
-
-    case SelectAttributeExpr(s, a) => {
-      val elems = elem.select(s)
-      if (elems.size == 0) ""
-      else elems.get(0).attr(a)
-    }
-    case ConcatExpr(a, b) =>
-      apply(a, elem, ctx) + apply(b, elem, ctx)
-    case IdentExpr(id) =>
-      ctx.ident(id)
-  }
-}
-  
-//SEARCH SUBTITLE AT "SubtitleSource.org"
-//  BROWSE "http://www.subtitlesource.org/search/" + SEARCH-TERM
-//  SELECT "#searchPage li a"
-//  BROWSE "http://www.subtitlesource.org/" + ATTRIBUTE "href"
-//  SELECT "ul#subtitle-list li"
-//  
-//  EXTRACT SUBTITLE-DOWNLOAD FROM "a:eq(1)" ATTRIBUTE "href" 
-//  EXTRACT SUBTITLE-PAGE FROM "a:eq(2)" ATTRIBUTE "href" 
-//  EXTRACT SUBTITLE-LANGUAGE FROM "a:eq(2)" ATTRIBUTE "title" 
-//ENDSEARCH
-
-object DSL extends StandardTokenParsers {
-  def parse = null
-  
-  def apply(s: String): Unit = {
-    val tokens = new lexical.Scanner(s)
-    phrase(parse)(tokens)
-  }
 }
 
 
 object DSL_Test {
+  val scraper1 = """
+SCRAPE SUBTITLES AT "SubtitleSource.org" BY "fizzl@foo"
+ 
+  BROWSE "http://www.subtitlesource.org/search/" + ATTRIBUTE "ficken" + <QUERY> 
+  SELECT "#searchPage li a"
+    
+    # GET "http://www.subtitlesource.org/" + ATTRIBUTE href
+    # BROWSE ATTRIBUTE "href" AS FULL URL
+    BROWSE "http://www.subtitlesource.org/" + ATTRIBUTE href
+    SELECT "ul#subtitle-list li"
+  
+      EXTRACT SUBTITLE-DOWNLOAD_URL SELECT-ATTRIBUTE "a:eq(1)" href 
+      EXTRACT SUBTITLE-PAGE_URL     SELECT-ATTRIBUTE "a:eq(2)" href 
+      EXTRACT SUBTITLE-LANGUAGE_TITLE SELECT-ATTRIBUTE "a:eq(2)" title
+      //EXTRACT SUBTITLE-LANGUAGE SELECT-ATTRIBUTE "a:eq(2)" title
+END"""
+  
   def main(args: Array[String]): Unit = {
+    val mdl = DSL(scraper1) 
     
-//    Search("SubtitleSource.org",
-//        Select("#searchPage li a[href]",
-//            Browse(
-//              Terminal
-//            )
-//          )
-//        ).search("foo")
-//      
-//      Thread.sleep(100000)
-    
-//    DSL("""
-//SEARCH SUBTITLES AT "SubtitleSource.org"
-//  GET "http://www.subtitlesource.org/search/$$$"
-//
-//  SELECT "#searchPage li"
-//  SELECT "a[href]"
-//  GET SELECTED
-//  
-//  SELECT "table#release-list tr td#subtitle-container"
-//   
-//  SELECT "ul#subtitle-list li"
-//  
-//  SELECT "a:eq(1)[href]"  AS SUBTITLE-DOWNLOAD
-//  SELECT "a:eq(2)[href]"  AS SUBTITLE-PAGE
-//  SELECT "a:eq(2)[title]" AS SUBTITLE-LANGUAGE
-//ENDSEARCH""")
+    println( mdl )
   
   }
 }
